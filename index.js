90dbe4b5e68296c9d367129922b70b8adc950d4d
Lillith3252 <74218901+Lilly3252@users.noreply.github.com>
2021-08-25 22:41:52 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-const LillyClient = require('./Structures/MenuDocsClient');
-const config = require('../config.json');
-
+const LillyClient = require("../src/Structures/LillyClient");
+const config = require("./config.json");
 const client = new LillyClient(config);
+const mongoose = require("mongoose");
+
+
+
+
+mongoose.connect(config.mongooseLink,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  }
+);
+console.log("im connected !");
+
 client.start();
