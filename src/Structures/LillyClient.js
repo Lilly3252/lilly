b3c46f770dbd0be3a135cfe7d6465307e9f79e60
Lillith3252 <74218901+Lilly3252@users.noreply.github.com>
2021-08-25 23:13:19 -0400

    Update LillyClient.js

--- src/Structures/LillyClient.js
+++ src/Structures/LillyClient.js
@@ -1,40 +1,52 @@
-const { Client, Collection, Permissions } = require('discord.js');
-const Util = require('./Util.js');
+const { Client, Collection, Permissions, Intents } = require("discord.js");
+const Util = require("./Util.js");
+const fs = require("fs");
+module.exports = class extends Client {
+  constructor(a = {}) {
+    super({
+      disableMentions: "everyone",
+      partials: ["MESSAGE", "CHANNEL", "REACTION", "USER", "GUILD_MEMBER"],
+      intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MEMBERS,
+        Intents.FLAGS.GUILD_BANS,
+        Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+        Intents.FLAGS.GUILD_INTEGRATIONS,
+        Intents.FLAGS.GUILD_WEBHOOKS,
+        Intents.FLAGS.GUILD_INVITES,
+        Intents.FLAGS.GUILD_VOICE_STATES,
+        Intents.FLAGS.GUILD_PRESENCES,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+        Intents.FLAGS.GUILD_MESSAGE_TYPING,
+        Intents.FLAGS.DIRECT_MESSAGES,
+        Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+        Intents.FLAGS.DIRECT_MESSAGE_TYPING,
+      ],
+    });
+    this.validate(a);
 
-module.exports = class LillyClient extends Client {
+    this.commands = new Collection();
+    this.aliases = new Collection();
+    this.events = new Collection();
+    this.queue = new Map();
+    this.utils = new Util(this);
+    this.owners = a.owners;
+  }
 
-	constructor(options = {}) {
-		super({
-			disableMentions: 'everyone'
-		});
-		this.validate(options);
-		this.commands = new Collection();
-		this.aliases = new Collection();
-		this.events = new Collection();
-        this.queue = new Map()
-		this.utils = new Util(this);
-		this.owners = options.owners;
-	}
-
-	validate(options) {
-		if (typeof options !== 'object') throw new TypeError('Options should be a type of Object.');
-
-		if (!options.token) throw new Error('You must pass the token for the client.');
-		this.token = options.token;
-
-		if (!options.prefix) throw new Error('You must pass a prefix for the client.');
-		if (typeof options.prefix !== 'string') throw new TypeError('Prefix should be a type of String.');
-		this.prefix = options.prefix;
-		
-		if (!options.defaultPerms) throw new Error('You must pass default perm(s) for the Client.');
-		this.defaultPerms = new Permissions(options.defaultPerms).freeze();
-	}
-
-	async start(token = this.token) {
-		this.utils.loadCommands();
-		this.utils.loadEvents();
-		
-		await super.login(token);
-	}
-
+  validate(a) {
+    if ("object" != typeof a)
+      throw new TypeError("Options should be a type of Object.");
+    if (!a.token) throw new Error("You must pass the token for the client.");
+    if (((this.token = a.token), !a.prefix))
+      throw new Error("You must pass a prefix for the client.");
+    if ("string" != typeof a.prefix)
+      throw new TypeError("Prefix should be a type of String.");
+    if (((this.prefix = a.prefix), !a.defaultPerms))
+      throw new Error("You must pass default perm(s) for the Client.");
+    this.defaultPerms = new Permissions(a.defaultPerms).freeze();
+  }
+  async start(a = this.token) {
+    this.utils.loadCommands(), this.utils.loadEvents(), await super.login(a);
+  }
 };
