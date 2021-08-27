f6351ac3d96ed344d160d5e99bafa0f0b3dfd103
Lillith3252 <74218901+Lilly3252@users.noreply.github.com>
2021-08-27 14:57:42 -0400

    Update messageCreate.js

--- src/Events/Message/messageCreate.js
+++ src/Events/Message/messageCreate.js
@@ -7,22 +7,19 @@ const { MessageEmbed } = require("discord.js");
 // Line 83 to 125 credit to ShinoTheShino
 
 module.exports = class extends Event {
-  async run(a) {
-    // ! a is message here !
-
-    if (/*!a.guild || */ a.author.bot) return;
+  async run(message) {
+    if (/*!message.guild || */ message.author.bot) return;
     // checking the dbc
-    if(a.channel.type = "DM"){
-      
-      console.log(a.content)
+    if ((message.channel.type = "DM")) {
+      console.log(message.content);
     }
-    
-    const b = await Guild.findOne({ guildID: a.guild.id }, (b, c) => {
+
+    const b = await Guild.findOne({ guildID: message.guild.id }, (b, c) => {
       if ((b && console.error(b), !c)) {
         const b = new Guild({
           _id: mongoose.Types.ObjectId(),
-          guildID: a.guild.id,
-          guildName: a.guild.name,
+          guildID: message.guild.id,
+          guildName: message.guild.name,
           prefix: config.prefix,
           moderatorRoleID: null,
           welcomechannelID: null,
@@ -38,7 +35,7 @@ module.exports = class extends Event {
             .save()
             .then((a) => console.log(a))
             .catch((a) => console.error(a)),
-          a.channel
+          message.channel
             .send(
               "This server was not in our database! We have now added and you should be able to use bot commands."
             )
@@ -57,25 +54,25 @@ module.exports = class extends Event {
     }*/
     // * mentions , bot , bot role and moderator role
     const c = { ignoreEveryone: !0 };
-    `<@!${a.guild.me.id}>` === a.content &&
-      a.channel.send(`My commands are Slash commands now.`),
-      a.mentions.has("775530518485270559", c) &&
-        a.channel.send(`Role mentioned , what's up?`);
+    `<@!${message.guild.me.id}>` === message.content &&
+      message.channel.send(`My commands are Slash commands now.`),
+      message.mentions.has("775530518485270559", c) &&
+        message.channel.send(`Role mentioned , what's up?`);
 
     const d = b.moderatorRoleID;
     const e = b.logchannelID;
     if (
-      a.mentions.has(d, c) &&
-      (a.client.channels.cache.get(e).send(
+      message.mentions.has(d, c) &&
+      (message.client.channels.cache.get(e).send(
         new MessageEmbed()
           .setTitle("Moderator Mentioned")
           .setThumbnail(a.guild.iconURL({ dynamic: !0 }))
           .setDescription([
-            `**Person who mentioned**: ${a.member}`,
-            `**Channel**: ${a.channel}`,
-            `**Content**: ${a.content}`,
+            `**Person who mentioned**: ${message.member}`,
+            `**Channel**: ${message.channel}`,
+            `**Content**: ${message.content}`,
           ])
-          .addField("\u200B", `[Click here to see the message](${a.url})`)
+          .addField("\u200B", `[Click here to see the message](${message.url})`)
       ),
       !e && !d)
     )
@@ -84,18 +81,17 @@ module.exports = class extends Event {
     //*command (something) detection
 
     // TODO : Do i really need that ? //*everything under that has been moved to interactionCreate
-    if (!this.client.application?.owner) await this.client.application?.fetch();
+    // if (!this.client.application?.owner) await this.client.application?.fetch();
 
     if (
-      (a.content.toLowerCase() === "!deploy" &&
-        a.author.id === this.client.application?.owner.id)
+      message.content.toLowerCase() === "!deploy" &&
+      message.author.id === this.client.application?.owner.id
     ) {
       // remove the || true when done lol
       // const data = {
       // 	name: 'ping',
       // 	description: 'Replies with Pong!',
       // };
-
       /*const commands = [
       {
         name: 'ping',
@@ -121,18 +117,16 @@ module.exports = class extends Event {
       }
     ];
     */
-      const commands = [...this.client.commands.values()].map((command) => ({
-        name: command.name,
-        description: command.description?.trim(), //  (command.description.substr(0, 97) + command.description.length > 97 ? '...' : '') : 'No description!',
-        //options: command.options || [],
-      }));
-
+      //const commands = [...this.client.commands.values()].map((command) => ({
+      //name: command.name,
+      //description: command.description?.trim(), //  (command.description.substr(0, 97) + command.description.length > 97 ? '...' : '') : 'No description!',
+      //options: command.options || [],
+      //}));
       //await this.client.application.commands.set(commands);
-      console.log(`registered ${commands.length} slash commands!`);
-      
-       const guild = await this.client.guilds.fetch('751501408213401650', { force: true }); // that's the id of the test server
-       await guild.commands.set(commands);
-       console.log(commands);
+      //console.log(`registered ${commands.length} slash commands!`);
+      //const guild = await this.client.guilds.fetch('751501408213401650', { force: true }); // that's the id of the test server
+      //await guild.commands.set(commands);
+      //console.log(commands);
     }
     //const f = b.prefix;
     //if (!a.content.startsWith(f)) return;
@@ -143,21 +137,21 @@ module.exports = class extends Event {
 
     //* verification for commands if owner only , guild , if nsfw command or if args are required
 
-    /*if (i) {
-      if (i.ownerOnly && !this.client.utils.checkOwner(a.author.id))
+    if (i) {
+      if (i.ownerOnly && !this.client.utils.checkOwner(message.author.id))
         return a.reply(
           "Sorry, this command can only be used by the bot owners."
         );
-      if (i.guildOnly && !a.guild)
+      if (i.guildOnly && !message.guild)
         return a.reply(
           "Sorry, this command can only be used in a discord server."
         );
-      if (i.nsfw && !a.channel.nsfw)
+      if (i.nsfw && !message.channel.nsfw)
         return a.reply(
           "Sorry, this command can only be ran in a NSFW marked channel."
         );
-      if (i.args && !h.length)
-        return a.reply(
+      if (i.args)
+        return message.reply(
           `Sorry, this command requires arguments to function. Usage: ${
             i.usage
               ? `${this.client.prefix + i.name} ${i.usage}`
@@ -165,15 +159,15 @@ module.exports = class extends Event {
           }`
         );
 
-        //* permission verification
-      if (a.guild) {
+      //* permission verification
+      if (message.guild) {
         const b = i.userPerms
           ? this.client.defaultPerms.add(i.userPerms)
           : this.client.defaultPerms;
         if (b) {
-          const c = a.channel.permissionsFor(a.member).missing(b);
+          const c = message.channel.permissionsFor(message.member).missing(b);
           if (c.length)
-            return a.reply(
+            return message.reply(
               `You are missing ${this.client.utils.formatArray(
                 c.map(this.client.utils.formatPerms)
               )} permissions, you need them to use this command!`
@@ -183,16 +177,16 @@ module.exports = class extends Event {
           ? this.client.defaultPerms.add(i.botPerms)
           : this.client.defaultPerms;
         if (c) {
-          const b = a.channel.permissionsFor(this.client.user).missing(c);
+          const b = message.channel.permissionsFor(this.client.user).missing(c);
           if (b.length)
-            return a.reply(
+            return message.reply(
               `I am missing ${this.client.utils.formatArray(
                 b.map(this.client.utils.formatPerms)
               )} permissions, I need them to run this command!`
             );
         }
       }
-    i.run(a, h);
+      i.run(message);
     }
-  */};
+  }
 };
