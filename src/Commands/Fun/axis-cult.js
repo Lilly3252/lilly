39385fc2fff3d97f46f4a2936a159fe32b966be8
Lillith3252 <74218901+Lilly3252@users.noreply.github.com>
2022-02-07 22:45:54 -0500

    Delete axis-cult.js

--- src/Commands/Fun/axis-cult.js
+++
@@ -1,9 +0,0 @@
-const { SlashCommandBuilder } = require("@discordjs/builders");
-const teachings = require("../../Structures/JSONs/axis-cult.json");
-
-module.exports = {
-	data: new SlashCommandBuilder().setName("axis-cult").setDescription("no description needed i think."),
-	async run(interaction) {
-		return interaction.reply(teachings[Math.floor(Math.random() * teachings.length)]);
-	}
-};
