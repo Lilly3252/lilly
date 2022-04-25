6f9f0a802633224feda38527d3984f07d081b4a3
Lilith <74218901+Lilly3252@users.noreply.github.com>
2022-04-24 22:40:32 -0400

    Update OwnerOnly.ts

--- src/preconditions/OwnerOnly.ts
+++ src/preconditions/OwnerOnly.ts
@@ -1,10 +1,11 @@
+require("dotenv").config();
 import { Precondition } from "@sapphire/framework";
 import type { Message } from "discord.js";
 import SYSTEM from "../structure/messageSystem.json";
 
 export class OwnerOnlyPrecondition extends Precondition {
   public run(message: Message) {
-    return message.author.id === "165922734461812736"
+    return message.author.id === process.env.OWNER
       ? this.ok()
       : this.error({
           message: SYSTEM.ERROR.COMMANDS.OWNER_ONLY,
