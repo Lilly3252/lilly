47b252f0e02cfebf8e16512cdc5ddafad05d6afe
Lilith <74218901+Lilly3252@users.noreply.github.com>
2022-04-25 21:26:05 -0400

    Add files via upload

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,4 @@
 require("dotenv").config();
-import { SapphireClient, BucketScope } from '@sapphire/framework';
-const client = new SapphireClient({
-  presence: {
-    activities: [{ name: "Will i keep that?", type: "WATCHING" }],
-  },
-  intents: [
-    "GUILDS",
-    "GUILD_MESSAGES",
-    "GUILD_MEMBERS",
-    "GUILD_PRESENCES",
-    "GUILD_VOICE_STATES",
-  ],
-  partials: ["MESSAGE", "CHANNEL", "REACTION", "USER", "GUILD_MEMBER"],
-  defaultCooldown: {
-    delay: 10_000, // 10_000 milliseconds
-    filteredCommands: ["ping"], // Ignore the `ping` command
-    filteredUsers: ["YOUR_ID"], // Ignore the bot owner
-    limit: 2, // Allow 2 uses before ratelimiting
-    scope: BucketScope.Channel, // Scope cooldown to channel
-  },
-  shards: "auto",
-  
-  }
-);
+import { Lillyclient } from './structure/Lillyclient';
+const client = new Lillyclient();
 client.login(process.env.TOKEN);
