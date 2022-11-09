cc44851b6152db8ab817ccc2e2da8636222cb8cc
Lilith <74218901+Lilly3252@users.noreply.github.com>
2022-11-09 10:58:30 -0500

    Delete src/src directory

--- src/src/events/ready.ts
+++
@@ -1,9 +0,0 @@
-import type { event } from "../structures/@types/index.js"
-
-
-export const name: event['name'] = 'ready';
-export const once: event["once"] = true
-
-export const run: event["run"] = async (): Promise<void> => {
-    console.log("Ready !!!!")
-}
