e578345c57f83e9b5890b3454896b95848f1eafa
Lilith <74218901+Lilly3252@users.noreply.github.com>
2022-11-30 01:00:52 -0500

    Delete src/events/guild directory

--- src/events/guild/guildMemberRemove.ts
+++
@@ -1,9 +0,0 @@
-import type { event } from '../../structures/index.js';
-import type { GuildMember } from 'discord.js';
-
-export const name: event['name'] = 'guildMemberRemove';
-export const once: event['once'] = false;
-
-export const run: event['run'] = async (member: GuildMember): Promise<void> => {
-	console.log(`${member.user.tag} removed`);
-};
