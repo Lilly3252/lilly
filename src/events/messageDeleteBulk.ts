48edda59479f78b75ef26f0ee03f6d53a12d1600
Lilith <74218901+Lilly3252@users.noreply.github.com>
2023-01-01 23:12:20 -0500

    Update messageDeleteBulk.ts

--- src/events/messageDeleteBulk.ts
+++ src/events/messageDeleteBulk.ts
@@ -1,35 +1,29 @@
 import type { event } from '../structures/@types/index.js';
-import type { Message, Snowflake } from 'discord.js';
+import type { GuildTextBasedChannel, Message, Snowflake } from 'discord.js';
 import type { Collection } from 'discord.js';
 import Guild from './../database/guildSettings.js';
 import { messageDeleteBulkEmbed } from '../structures/messageEmbeds.js';
 export const name: event['name'] = 'messageDeleteBulk';
 export const once: event['once'] = false;
 
-export const run: event['run'] = async (messages: Collection<Snowflake, Message<true>>): Promise<any> => {
-	const messageCollection = messages.first();
-	const settings = await Guild.findOne({
-		guildID: messageCollection?.guild?.id,
-	});
+export const run: event['run'] = async (messages: Collection<Snowflake, Message<true>> , channel:GuildTextBasedChannel): Promise<any> => {
+	const settings = await Guild.findOne({guildID: messages.first()?.guild?.id});
 
-	if (settings?.messageBulkDeleteMode === false) {
-		return;
-	}
-	if (!messageCollection?.author) {
+	if (!settings?.messageBulkDeleteMode || !messages.first()?.author ) {
 		return;
 	}
 	const length = [...messages.values()].length;
 
-	const deletedMessageChannel = messageCollection?.channel.name;
+	const deletedMessageChannel = channel;
 
-	const moderatorChannel = settings?.logChannelID;
+	const moderatorChannel = settings.logChannelID;
 
-	if (!moderatorChannel || moderatorChannel === null) return;
-	const logChannel = messageCollection?.client.channels.cache.get(moderatorChannel);
-	if (!logChannel || logChannel === null) {
+	if (!moderatorChannel) return;
+	const logChannel = messages.first()?.client.channels.cache.get(moderatorChannel);
+	if (!logChannel) {
 		return;
 	}
-	if (logChannel?.isTextBased()) {
+	if (logChannel.isTextBased()) {
 		logChannel.send({ embeds: [messageDeleteBulkEmbed(messages, deletedMessageChannel, length)] });
 	}
 };
