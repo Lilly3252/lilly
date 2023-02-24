4588c591af67d2952c3ad77483044984d33c13fe
Lilith <74218901+Lilly3252@users.noreply.github.com>
2023-02-24 17:57:26 -0500

    Update setTag.ts

--- src/commands/administrator/setTag.ts
+++ src/commands/administrator/setTag.ts
@@ -3,7 +3,7 @@
 import type { ChatInputCommandInteraction } from "discord.js";
 import type { SlashCommand } from "../../structures/@types/index.js";
 import { PermissionsBitField, SlashCommandBuilder } from "discord.js";
-//import { prisma } from '../../index.js';
+
 
 export const slashy: SlashCommand["slashy"] = new SlashCommandBuilder()
     .setName("settings-tags").setDescription("Settings for tags.")
@@ -18,39 +18,23 @@ export const run: SlashCommand["run"] = async (interaction: ChatInputCommandInte
     const choices = interaction.options.getString("actions")!
     const name = interaction.options.getString("name")!;
 //    const message = interaction.options.getString("message")!;
-    //const guild_db = prisma.tags
+   
     switch (choices) {
         case "create": {
-       //     await guild_db.create({
-         //       data: {
-           //         "guildID": interaction.guild.id,
-             //       "name": name,
-               //     "description": message,
-                 //   "username": interaction.user.username
-                }
-            //)
-            interaction.reply(`Successfully created a tag with ${name}`)
+
+        }
             break;
         case "delete": {
-           // await guild_db.delete({ where: { name: name } })
-            interaction.reply(`Successfully deleted a tag with ${name}`)
+          
         }
             break;
         case "modify": {
-            //await guild_db.update({
-              //  where: { name: name },
-               // data: {
-                 //   "name": name,
-                  //  "description": messageD
-                }
-            //)
-            interaction.reply(`Successfully modified a tag with ${name}`)
+        
+}
 
             break;
         case "listall": {
-//            const maptag = guild_db.findMany({ select: { name: true } })
-  //          console.log(maptag)
-            interaction.reply(`there is a list of all tags.`)
+//           
         }
             break;
     }
