aed0a85ad65f4c280de5340245ca4bbf532ef1c5
Lilith <74218901+Lilly3252@users.noreply.github.com>
2023-07-15 14:55:24 -0400

    Update eventEmbeds.ts
    AuditLogEmbed update

--- src/structures/embeds/eventEmbeds.ts
+++ src/structures/embeds/eventEmbeds.ts
@@ -1,7 +1,7 @@
 import * as diff from 'diff';
 import {
-	type AuditLogChange, AuditLogEvent, AutoModerationRule, BaseChannel, Collection, EmbedBuilder, escapeMarkdown, Guild, type GuildAuditLogsActionType, GuildAuditLogsEntry, type GuildAuditLogsTargetType, type GuildTextBasedChannel, Integration,
-	Invite, Message, Role, type Snowflake, TextChannel, User,
+	ApplicationCommand, type AuditLogChange, AuditLogEvent, AutoModerationRule, BaseChannel, Collection, EmbedBuilder, escapeMarkdown, Guild, type GuildAuditLogsActionType, GuildAuditLogsEntry, type GuildAuditLogsTargetType, type GuildTextBasedChannel,
+	Integration, Invite, Message, Role, type Snowflake, TextChannel, User,
 } from 'discord.js';
 
 export function messageDeleteEmbed(message: Message) {
@@ -110,50 +110,99 @@ export function messageUpdateEmbed(oldMessage: Message<true>, newMessage: Messag
 	return messageUpEmbed;
 }
 export function auditLogEmbed(auditLogEntry: GuildAuditLogsEntry, fetchedAuditLog: GuildAuditLogsEntry<null, GuildAuditLogsActionType, GuildAuditLogsTargetType, AuditLogEvent> | undefined, actionExecuted: string, guild: Guild) {
+	function isUndefined(value: any) {
+		if (value === undefined) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
 	const autoModerationEmbed = new EmbedBuilder().setTitle(actionExecuted).setAuthor({ name: `${auditLogEntry.executor?.username} (${auditLogEntry.executorId})` });
 
 	const target = fetchedAuditLog?.target;
 	const auditLogtargetType = fetchedAuditLog?.targetType;
 	const change = fetchedAuditLog?.changes[0];
 	const extra = auditLogEntry.extra!;
-
 	const auditLogEntryChangesKeyName = auditLogEntry.changes[0]?.key.replaceAll('_', ' ');
 	const auditLogActionType = fetchedAuditLog?.actionType;
+	let x = '';
+	switch (auditLogtargetType) {
+		case 'Channel':
+			x = '#';
+			break;
+		case 'Role':
+			x = '&';
+			break;
+		case 'User':
+			x = '@';
+	}
 	const changesDone = auditLogEntry.changes
 		.map((changes: AuditLogChange) => {
-			return `**❯** ${guild.client.utils.toTitleCase(changes.key.replaceAll('_', ' '))}  \n \u3000 New: ${
-				// change every first key to put a uppercase
-				Array.isArray(changes.new) // If its array
-					? changes.new.map(
-							(
-								element, // map that
-							) =>
-								Object.entries(element) // do another array inside that array
-									.map(([key, value]) => `\n\u3000 ${key}: ${value}`) //map that array too .. with a key and a value
-									.join(' '), // join those with a ,
+			return `**❯** ${guild.client.utils.toTitleCase(changes.key.replaceAll('_', ' '))}   \n\u3000 New: ${
+				Array.isArray(changes.new)
+					? changes.new.map((element) =>
+							Object.entries(element)
+								.map(([key, value]) => `\n\u3000 ${key}: ${value}`)
+								.join(' '),
 					  )
 					: changes.new
-			}\n \u3000 Old: ${
+			} \n\u3000 Old:${
 				Array.isArray(changes.old)
 					? changes.old.map((element) =>
 							Object.entries(element)
 								.map(([key, value]) => `\n\u3000 ${key}: ${value}`)
 								.join(' '),
 					  )
-					: `\n\u3000 ${changes.old}`
+					: changes.old
 			}`;
 		})
 		.join('\n');
-	//JSON.stringify(changes.new,  ["id", "allow"], "\t").replaceAll(/\{\n\s+|\n\s+\}|\[|\]$/g, '\t')
-	//.replaceAll(/[^a-z,1-9:0]/g," ") <-- To keep for in case i need that ..
+
+	const changesDoneNew = auditLogEntry.changes
+		.map((changes: AuditLogChange) => {
+			return `**❯** ${guild.client.utils.toTitleCase(changes.key.replaceAll('_', ' '))}   \n\u3000 New: ${
+				Array.isArray(changes.new)
+					? changes.new.map((element) =>
+							Object.entries(element)
+								.map(([key, value]) => `\n\u3000 ${key}: ${value}`)
+								.join(' '),
+					  )
+					: changes.new
+			}`;
+		})
+		.join('\n');
+	const changesDoneOld = auditLogEntry.changes
+		.map((changes: AuditLogChange) => {
+			return `**❯** ${guild.client.utils.toTitleCase(changes.key.replaceAll('_', ' '))} \n\u3000 Old:${
+				Array.isArray(changes.old)
+					? changes.old.map((element) =>
+							Object.entries(element)
+								.map(([key, value]) => `\n\u3000 ${key}: ${value}`)
+								.join(' '),
+					  )
+					: changes.old
+			}`;
+		})
+		.join('\n');
+		function changes(_target:GuildAuditLogsTargetType):void{
+			if (change) {
+				autoModerationEmbed.addFields({ name: 'Changes:', value: `${isUndefined(change?.new) ? changesDoneOld : isUndefined(change.old) ? changesDoneNew : changesDone}` });
+			}
+		}
 
 	if (target instanceof AutoModerationRule) {
 		autoModerationEmbed.addFields({ name: 'Action done:', value: `${auditLogActionType} on ${target.name}` });
-		if (change) {
-			autoModerationEmbed.addFields({ name: 'Changes:', value: `${changesDone}` });
-		}
+		changes("AutoModerationRule")
 		return autoModerationEmbed;
 	}
+
+	if (target instanceof ApplicationCommand) {
+		autoModerationEmbed.addFields({ name: 'Action done:', value: `${auditLogActionType} on ${target.name}` });
+		changes("ApplicationCommand")
+		return autoModerationEmbed;
+	}
+
 	if (target instanceof User) {
 		if (!target) {
 			return autoModerationEmbed;
@@ -162,7 +211,7 @@ export function auditLogEmbed(auditLogEntry: GuildAuditLogsEntry, fetchedAuditLo
 		if (extra) {
 			const extraobject = Object.entries(Object.entries(extra)[0]!);
 			const Extra = extraobject[1]?.[1];
-			const ExtraMapped = Object.entries(Extra);
+			//const ExtraMapped = Object.entries(Extra);
 
 			//console.log(Array.isArray(ExtraMapped)? ExtraMapped.map(([key, value]) => ` \u3000 \u3000 ${key}: ${value}`) : "Not a array dumbass")
 			if (Extra instanceof TextChannel) {
@@ -182,53 +231,41 @@ export function auditLogEmbed(auditLogEntry: GuildAuditLogsEntry, fetchedAuditLo
 			}
 			//TODO : Extra.messages doesnt show content , need fix.
 		}
-		if (change) {
-			autoModerationEmbed.addFields({ name: 'Changes:', value: `${changesDone}` });
-		}
+		changes("User")
 		return autoModerationEmbed;
 	}
 	if (auditLogtargetType === 'Channel' && auditLogActionType === 'Delete') {
-		autoModerationEmbed.addFields({ name: 'Action done:', value: `${auditLogEntry.actionType} a channel with Id: ${auditLogEntry.targetId}` });
+		autoModerationEmbed.addFields({ name: 'Action done:', value: `${auditLogEntry.actionType} channel: <${x}${auditLogEntry.targetId}>` });
 		if (change) {
-			autoModerationEmbed.addFields({ name: 'Changes:', value: `${changesDone}` });
+			autoModerationEmbed.addFields({ name: 'Changes:', value: `${isUndefined(change?.new) ? changesDoneOld : isUndefined(change.old) ? changesDoneNew : changesDone}` });
 		}
 	}
 	if (target instanceof BaseChannel) {
 		if (target.isTextBased()) {
-			autoModerationEmbed.addFields({ name: 'Action done:', value: `${auditLogEntry.actionType} on ${target.name} (${target.id})` });
-			if (change) {
-				autoModerationEmbed.addFields({ name: 'Changes:', value: `${changesDone}` });
-			}
+			autoModerationEmbed.addFields({ name: 'Action done:', value: `${auditLogEntry.actionType} channel: <${x}${target.id}> (${target.id})` });
+			changes("Channel")
 			return autoModerationEmbed;
 		}
 		return autoModerationEmbed;
 	}
 	if (target instanceof Guild) {
 		autoModerationEmbed.addFields({ name: 'Action done:', value: `${auditLogEntry.actionType} ${auditLogEntryChangesKeyName?.replaceAll('_', ' ')} on ${target.name} (${target.id})` });
-		if (change) {
-			autoModerationEmbed.addFields({ name: 'Changes:', value: `${changesDone}` });
-		}
+		changes("Guild")
 		return autoModerationEmbed;
 	}
 	if (target instanceof Integration) {
 		autoModerationEmbed.addFields({ name: 'Action done:', value: `${auditLogEntry.actionType} on ${target.name} (${target.id})` });
-		if (change) {
-			autoModerationEmbed.addFields({ name: 'Changes:', value: `${changesDone}` });
-		}
+		changes("Integration")
 		return autoModerationEmbed;
 	}
 	if (target instanceof Role) {
 		autoModerationEmbed.addFields({ name: 'Action done:', value: `${auditLogEntry.actionType} on ${target.name} (${target.id})` });
-		if (change) {
-			autoModerationEmbed.addFields({ name: 'Changes:', value: `${changesDone}` });
-		}
+		changes("Role")
 		return autoModerationEmbed;
 	}
 	if (target instanceof Invite) {
 		autoModerationEmbed.addFields({ name: 'Action done:', value: `${auditLogEntry.actionType} on ${target.code}` });
-		if (change) {
-			autoModerationEmbed.addFields({ name: 'Changes:', value: `${changesDone}` });
-		}
+		changes("Invite")
 		return autoModerationEmbed;
 	}
 	return autoModerationEmbed;
