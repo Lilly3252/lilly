e7a8666023c9f9753f03991efd959f0f01ba5014
Lilith <74218901+Lilly3252@users.noreply.github.com>
2023-10-03 18:12:32 -0400

    Delete src directory

--- src/database/blacklistUser.ts
+++
@@ -1,9 +0,0 @@
-import type { blacklistUsers } from '#type/database.js';
-import { model, Schema } from 'mongoose';
-
-const blacklist = new Schema<blacklistUsers>({
-	guildID: { type: String },
-	ID: { type: String },
-	reason: { type: String },
-});
-export default model<blacklistUsers>('userBlacklist', blacklist);
--- src/events/guildMemberRemove.ts
+++
@@ -1,9 +0,0 @@
-import type { event } from '#type/index.js';
-import type { GuildMember } from 'discord.js';
-
-export const name: event['name'] = 'guildMemberRemove';
-export const once: event['once'] = false;
-
-export const run: event['run'] = async (member: GuildMember): Promise<any> => {
-	console.log(`${member.user.tag} removed`);
-};
