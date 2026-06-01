8a6b8e3e10ba3cec46c1d927dfcf8213015f48ba
ShompiFlen <shompire02@gmail.com>
2026-06-01 13:48:16 -0400

    feat: Added spanish LATAM translations for this command

--- src/slashyInformations/moderation/kick.ts
+++ src/slashyInformations/moderation/kick.ts
@@ -1,10 +1,11 @@
-import { ApplicationCommandOptionType } from "discord-api-types/v10";
+import { ApplicationCommandOptionType, Locale } from "discord-api-types/v10";
 export const KickCommand = {
 	name: "kick",
 	description: "Kick a user.",
 	description_localizations: {
 		fr: "Kick un utilisateur",
-		ja: "ユーザーをキックする"
+    ja: "ユーザーをキックする",
+    [Locale.SpanishLATAM]: "Expulsar a un usuario"
 	},
 	options: [
 		{
@@ -12,12 +13,14 @@ export const KickCommand = {
 			name: "target",
 			name_localizations: {
 				fr: "target",
-				ja: "ターゲット"
+        ja: "ターゲット",
+				[Locale.SpanishLATAM]: "usuario"
 			},
 			description: "Select a user to kick",
 			description_localizations: {
 				fr: "Sélectionner l'utilisateur à kick",
-				ja: "キックするユーザーを選択する"
+				ja: "キックするユーザーを選択する",
+				[Locale.SpanishLATAM]: "El usuario al que quieres expulsar"
 			},
 			required: true
 		},
@@ -26,12 +29,14 @@ export const KickCommand = {
 			name: "reason",
 			name_localizations: {
 				fr: "raison",
-				ja: "理由"
+        ja: "理由",
+				[Locale.SpanishLATAM]: "razon"
 			},
 			description: "Reason of the kick",
 			description_localizations: {
 				fr: "Raison du kick",
-				ja: "キックの理由"
+        ja: "キックの理由",
+				[Locale.SpanishLATAM]: "Razón de la expulsión"
 			}
 		},
 		{
@@ -39,12 +44,14 @@ export const KickCommand = {
 			name: "hide",
 			name_localizations: {
 				fr: "masquer",
-				ja: "非表示"
+        ja: "非表示",
+				[Locale.SpanishLATAM]: "ocultar"
 			},
 			description: "Hides the output",
 			description_localizations: {
 				fr: "Masque(cacher) le résultat",
-				ja: "出力を非表示にする"
+				ja: "出力を非表示にする",
+				[Locale.SpanishLATAM]: "Ocultar la respuesta del comando"
 			}
 		}
 	],
