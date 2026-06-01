1f16956cc227ff9841247d9ea614dbee903164ba
ShompiFlen <shompire02@gmail.com>
2026-06-01 16:38:35 -0400

    feat: Added spanish LATAM translations for this command

--- src/slashyInformations/moderation/timeout.ts
+++ src/slashyInformations/moderation/timeout.ts
@@ -1,11 +1,12 @@
-import { ApplicationCommandOptionType } from "discord-api-types/v10";
+import { ApplicationCommandOptionType, Locale } from "discord-api-types/v10";
 
 export const TimeoutCommand = {
 	name: "timeout",
 	description: "Timeout a member.",
 	description_localizations: {
 		fr: "Mute un membre.",
-		ja: "メンバーをタイムアウトする。"
+    ja: "メンバーをタイムアウトする。",
+		[Locale.SpanishLATAM]: "Silenciar a un miembro"
 	},
 	options: [
 		{
@@ -13,12 +14,14 @@ export const TimeoutCommand = {
 			name: "target",
 			name_localizations: {
 				fr: "membre",
-				ja: "ターゲット"
+        ja: "ターゲット",
+				[Locale.SpanishLATAM]: "usuario"
 			},
 			description: "The member to timeout.",
 			description_localizations: {
 				fr: "Le membre à mute.",
-				ja: "タイムアウトするメンバー。"
+				ja: "タイムアウトするメンバー。",
+				[Locale.SpanishLATAM]: "El usuario a silenciar."
 			},
 			required: true
 		},
@@ -27,12 +30,14 @@ export const TimeoutCommand = {
 			name: "duration",
 			name_localizations: {
 				fr: "durée",
-				ja: "期間"
+        ja: "期間",
+				[Locale.SpanishLATAM]: "duracion"
 			},
 			description: "The duration of the timeout in seconds.",
 			description_localizations: {
 				fr: "La durée du mute en secondes.",
-				ja: "タイムアウトの期間（秒単位）。"
+				ja: "タイムアウトの期間（秒単位）。",
+				[Locale.SpanishLATAM]: "La duracion del silencio en segundos."
 			},
 			required: true
 		},
@@ -41,12 +46,14 @@ export const TimeoutCommand = {
 			name: "reason",
 			name_localizations: {
 				fr: "raison",
-				ja: "理由"
+				ja: "理由",
+				[Locale.SpanishLATAM]: "razon"
 			},
 			description: "The reason for the timeout.",
 			description_localizations: {
 				fr: "La raison du mute.",
-				ja: "タイムアウトの理由。"
+				ja: "タイムアウトの理由。",
+				[Locale.SpanishLATAM]: "La razon del silencio."
 			},
 			required: false
 		},
@@ -55,12 +62,14 @@ export const TimeoutCommand = {
 			name: "hide",
 			name_localizations: {
 				fr: "masquer",
-				ja: "非表示"
+				ja: "非表示",
+				[Locale.SpanishLATAM]: "ocultar"
 			},
 			description: "Hides the output.",
 			description_localizations: {
 				fr: "Masque le résultat.",
-				ja: "出力を非表示にする。"
+				ja: "出力を非表示にする。",
+				[Locale.SpanishLATAM]: "Ocultar la respuesta del comando"
 			},
 			required: false
 		}
