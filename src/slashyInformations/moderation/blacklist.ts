c4f9e72b7fdff0d64735ed2bb8b14f13c00df40f
ShompiFlen <shompire02@gmail.com>
2026-06-01 13:46:47 -0400

    feat: Added spanish LATAM translations to this command

--- src/slashyInformations/moderation/blacklist.ts
+++ src/slashyInformations/moderation/blacklist.ts
@@ -1,16 +1,18 @@
-import { ApplicationCommandOptionType } from "discord-api-types/v10";
+import { ApplicationCommandOptionType, Locale } from "discord-api-types/v10";
 
 export const BlacklistCommand = {
 	// pingCommand / banCommand / settingCommand ...
 	name: "blacklist",
 	name_localizations: {
 		fr: "liste-noire",
-		ja: "ブラックリスト"
+    ja: "ブラックリスト",
+		[Locale.SpanishLATAM]: "lista-negra"
 	},
 	description: "Add a user to the blacklist.",
 	description_localizations: {
 		fr: "Ajouter un membre à la liste noire",
-		ja: "ユーザーをブラックリストに追加します。"
+		ja: "ユーザーをブラックリストに追加します。",
+		[Locale.SpanishLATAM]: "Agrega un usuario a la lista negra."
 	},
 	options: [
 		{
@@ -18,12 +20,14 @@ export const BlacklistCommand = {
 			name: "target",
 			name_localizations: {
 				fr: "target",
-				ja: "ターゲット"
+        ja: "ターゲット",
+				[Locale.SpanishLATAM]: "usuario"
 			},
 			description: "User to be added",
 			description_localizations: {
 				fr: "Membre à ajouter",
-				ja: "追加するユーザー"
+        ja: "追加するユーザー",
+				[Locale.SpanishLATAM]: "Usuario al que quieres dejar en lista-negra"
 			},
 			required: true
 		},
@@ -32,12 +36,14 @@ export const BlacklistCommand = {
 			name: "hide",
 			name_localizations: {
 				fr: "masquer",
-				ja: "非表示"
+        ja: "非表示",
+				[Locale.SpanishLATAM]: "ocultar"
 			},
 			description: "Hides the output.",
 			description_localizations: {
 				fr: "Masque(cacher) le résultat.",
-				ja: "出力を非表示にします。"
+				ja: "出力を非表示にします。",
+				[Locale.SpanishLATAM]: "Oculta la respuesta de este comando."
 			}
 		}
 	],
